379d5f60a9e3b468241294f7787a2b8e84c5ef13
daws-76s-github <naveenkuduthuru>
2024-02-07 23:30:16 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 AMI=ami-0f3c7d07486cad139
-SG_ID=sg-087e7afb3a936fce7 #replace with your SG ID
+SG_ID=sg-01a6080de1a905872 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=# sg-01a6080de1a905872 #replace your zone ID
-DOMAIN_NAME="daws76s.space"
+
+
 
 for i in "${INSTANCES[@]}"
 do
@@ -14,27 +14,8 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
+    done 
 
-    #create R53 record, make sure you delete existing record
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Creating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$i'.'$DOMAIN_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP_ADDRESS'"
-            }]
-        }
-        }]
-    }
-        '
-done
+  
