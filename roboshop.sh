8ea66c94701b57dd82919268c9ede15fc4c40a21
daws-76s-github <naveenkuduthuru>
2024-02-07 23:47:52 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -15,7 +15,10 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-01a6080de1a905872
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro 
+    --security-group-ids sg-01a6080de1a905872 
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
+
     done 
 
   
