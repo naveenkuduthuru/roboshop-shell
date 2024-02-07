006e39f599b5c96709956a5c85e0296165cc3335
daws-76s-github <naveenkuduthuru>
2024-02-08 00:14:43 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -15,7 +15,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type
-    $INSTANCE_TYPE --security-group-ids sg-01a6080de1a905872 --tag-specification
+    $INSTANCE_TYPE --security-group-ids sg-01a6080de1a905872 --tag-specifications
     "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].
     PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
