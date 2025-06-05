0367dc59c606072830199bd30716bc8f4d984839
joindevops07 <naveen@devops.com>
2025-06-05 23:48:52 +0530

    shell varibles

--- roboshop.sh
+++ roboshop.sh
@@ -7,6 +7,7 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 
 for i in "${INSTANCES[@]}"
 do
+     echo "insatnce is: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
