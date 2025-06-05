38040c06f58b3cf04bd3245040a816d6fbc4fe97
joindevops07 <naveen@devops.com>
2025-06-06 00:18:42 +0530

    shell varibles

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 
 for i in "${INSTANCES[@]}"
 do
-     echo "insatnce is: $i"
+    echo "insatnce is: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
@@ -15,5 +15,5 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type t2.micro --security-group-ids sg-034ef8f5fd18dd359
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-034ef8f5fd18dd359
 done        
