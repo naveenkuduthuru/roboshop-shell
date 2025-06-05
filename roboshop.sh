0deb8805a49211a7e46d6d8be385ba928c6c7a45
joindevops07 <naveen@devops.com>
2025-06-05 23:44:18 +0530

    shell varibles

---
+++ roboshop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+AMI=ami-0b4f379183e5706b9 #this keeps on changing
+SG_ID=sg-034ef8f5fd18dd359 #replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type t2.micro --security-group-ids sg-034ef8f5fd18dd359
+done    
