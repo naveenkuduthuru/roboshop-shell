73beac8a3d5eab746f4772edbe65f77568b8bb3d
joindevops07 <naveen@devops.com>
2025-06-14 11:16:55 +0530

    shell varibles

--- redis.sh
+++ redis.sh
@@ -1,45 +1,57 @@
+
 #!/bin/bash
-START_TIME=$(date +%s)
-USERID=$(id -u)
+
+ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
-# check the user has root priveleges or not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi
-# validate functions takes input as exit status, what command they tried to install
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+exec &>$LOGFILE
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
 VALIDATE(){
-    if [ $1 -eq 0 ]
+    if [ $1 -ne 0 ]
     then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $R FAILED $N"
         exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
-dnf module disable redis -y &>>$LOG_FILE
-VALIDATE $? "Disabling Default Redis version"
-dnf module enable redis:7 -y &>>$LOG_FILE
-VALIDATE $? "Enabling Redis:7"
-dnf install redis -y &>>$LOG_FILE
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+VALIDATE $? "Installing Remi release"
+
+dnf module enable redis:remi-6.2 -y
+
+VALIDATE $? "enabling redis"
+
+dnf install redis -y
+
 VALIDATE $? "Installing Redis"
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Edited redis.conf to accept remote connections"
-systemctl enable redis &>>$LOG_FILE
-VALIDATE $? "Enabling Redis"
-systemctl start redis  &>>$LOG_FILE
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+
+VALIDATE $? "allowing remote connections"
+
+systemctl enable redis
+
+VALIDATE $? "Enabled Redis"
+
+systemctl start redis
+
 VALIDATE $? "Started Redis"
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
